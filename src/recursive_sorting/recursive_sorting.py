72639e38edaeb905a75cb9ed0cbe09456a89e9e6
Nckflannery <Nckflannery@users.noreply.github.com>
2020-03-17 16:30:17 -0700

    Stretch done

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -63,30 +63,37 @@ def merge_sort( arr ):
 
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
-    arrA = arr[start:mid]
-    arrB = arr[mid:end]
+    arr2_start = mid + 1; 
+  
+    if arr[mid] <= arr[arr2_start]: 
+        return
+      
+    while start <= mid and arr2_start <= end: 
+  
+        if arr[start] <= arr[arr2_start]: 
+            start += 1; 
+        else: 
+            value = arr[arr2_start]; 
+            index = arr2_start; 
+  
+            while index != start: 
+                arr[index] = arr[index - 1]; 
+                index -= 1; 
+              
+            arr[start] = value; 
 
-    output = []
-    i, j = 0, 0
-    while i < len(arrA) and j < len(arrB):
-        if arrA[i] < arrB[j]:
-            output.append(arrA[i])
-            i += 1
-        else:
-            output.append(arrB[j])
-            j += 1
-    output += arrA[i:]
-    output += arrB[j:]
-    return output
+            start += 1; 
+            mid += 1; 
+            arr2_start += 1; 
      
 
 def merge_sort_in_place(arr, l, r): 
     
-    if len(arr) > 1:
-        m = len(arr) // 2
-        left = merge_sort_in_place(arr, l, m)
-        right = merge_sort_in_place(arr, m, r)
-        return merge(left, right)
+    if l < r:
+      m = (l + r)//2
+      merge_sort_in_place(arr, l, m)
+      merge_sort_in_place(arr, m+1, r)
+      merge_in_place(arr, l, m, r)
     return arr
 
 
